c129f370674a536cba9565364c9f0514ed5ffc94
uskovvo <uskovvospb@gmail.com>
2022-09-26 20:44:23 +0200

    H2 DataBase added

--- HomeTask/simple_mvc/src/main/java/org/example/app/services/BookRepository.java
+++ HomeTask/simple_mvc/src/main/java/org/example/app/services/BookRepository.java
@@ -3,10 +3,14 @@ package org.example.app.services;
 import org.apache.log4j.Logger;
 import org.example.web.dto.Book;
 import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
@@ -15,66 +19,86 @@ import java.util.Objects;
 public class BookRepository implements ProjectRepository<Book>, ApplicationContextAware {
 
     private final Logger logger = Logger.getLogger(BookRepository.class);
-    private final List<Book> repo = new ArrayList<>();
     private ApplicationContext context;
+
+    private final NamedParameterJdbcTemplate jdbsTemplate;
+
+    @Autowired
+    public BookRepository(NamedParameterJdbcTemplate jdbsTemplate) {
+        this.jdbsTemplate = jdbsTemplate;
+    }
 
     @Override
     public List<Book> retreiveAll() {
-        return new ArrayList<>(repo);
+        List<Book> books = jdbsTemplate.query("SELECT * FROM books", (ResultSet rs, int rowNum) -> {
+            Book book = new Book();
+            book.setId(rs.getInt("id"));
+            book.setAuthor(rs.getString("author"));
+            book.setTitle(rs.getString("title"));
+            book.setSize(rs.getInt("size"));
+            return book;
+        });
+        return new ArrayList<>(books);
     }
 
     @Override
     public void store(Book book) {
         if(!book.getAuthor().isEmpty() || !book.getTitle().isEmpty() || book.getSize() != null) {
-            book.setId(context.getBean(IdProvider.class).provideId(book));
             logger.info("store new book: " + book);
-            repo.add(book);
+            MapSqlParameterSource parameterSource = new MapSqlParameterSource();
+            parameterSource.addValue("author", book.getAuthor());
+            parameterSource.addValue("title", book.getTitle());
+            parameterSource.addValue("size", book.getSize());
+            jdbsTemplate.update("INSERT INTO books(author, title, size) VALUES(:author, :title, :size)", parameterSource);
         }
     }
 
     @Override
     public boolean removeItemByRegex(String queryRegex) {
         if(!queryRegex.isEmpty()) {
+            MapSqlParameterSource parameterSource = new MapSqlParameterSource();
             queryRegex = queryRegex.trim();
             if (isDigit(queryRegex)) {
                 Integer intQuery = Integer.parseInt(queryRegex);
-                return removeItemBySize(intQuery);
+                return removeItemBySizeOrId(intQuery, parameterSource);
             }else {
-                return removeItemByAuthorOrTitleOrId(queryRegex);
+                return removeItemByAuthorOrTitle(queryRegex, parameterSource);
             }
         }
         return false;
     }
 
-    private boolean removeItemBySize(Integer intToRemove) {
-//        List<Book> newList = new ArrayList<>();
+    private boolean removeItemBySizeOrId(Integer intToRemove, MapSqlParameterSource parameterSource) {
         int count = 0;
         for(Book book: retreiveAll()){
             if(Objects.equals(book.getSize(), intToRemove)){
-                logger.info("remove book by Size: " + book);
+                parameterSource.addValue("size", intToRemove);
+                jdbsTemplate.update("DELETE FROM books WHERE size = :size", parameterSource);
                 count++;
-                repo.remove(book);
+                logger.info("remove book by Size: " + book);
+            }else if(Objects.equals(book.getId(), intToRemove)){
+                parameterSource.addValue("id", intToRemove);
+                jdbsTemplate.update("DELETE FROM books WHERE id = :id", parameterSource);
+                logger.info("remove book by Id: " + book);
+                return true;
             }
         }
         return count > 0;
     }
 
-    private boolean removeItemByAuthorOrTitleOrId(String stringToRemove){
-//        List<Book> newList = new ArrayList<>();
+    private boolean removeItemByAuthorOrTitle(String stringToRemove, MapSqlParameterSource parameterSource){
         int count = 0;
         for (Book book: retreiveAll()){
             if(book.getAuthor().equals(stringToRemove)){
+                parameterSource.addValue("author", stringToRemove);
+                jdbsTemplate.update("DELETE FROM books WHERE author = :author", parameterSource);
                 logger.info("remove book by Author: " + book);
                 count++;
-                repo.remove(book);
             }else if(book.getTitle().equals(stringToRemove)){
+                parameterSource.addValue("title", stringToRemove);
+                jdbsTemplate.update("DELETE FROM books WHERE title = :title", parameterSource);
                 logger.info("remove book by Title: " + book);
                 count++;
-                repo.remove(book);
-            }else if(book.getId().equals(stringToRemove)){
-                logger.info("remove book by Id: " + book);
-                repo.remove(book);
-                return true;
             }
         }
         return count > 0;
