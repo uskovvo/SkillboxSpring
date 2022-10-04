5e4954a58fefe7017b53e94b7ce91d6440c7ccbe
uskovvo <uskovvospb@gmail.com>
2022-10-04 20:26:21 +0200

    New front-end

--- MyBookShopApp/src/main/java/com/example/mybookshopapp/controllers/MainPageController.java
+++ MyBookShopApp/src/main/java/com/example/mybookshopapp/controllers/MainPageController.java
@@ -7,6 +7,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 @Controller
 @RequestMapping("/bookshop")
 public class MainPageController {
@@ -21,6 +24,10 @@ public class MainPageController {
     @GetMapping("/main")
     public String mainPage(Model model) {
         model.addAttribute("bookData", bookService.getBooksData());
+        model.addAttribute("searchPlaceholder", "new search placeholder");
+        model.addAttribute("serverTime", new SimpleDateFormat("hh:mm:ss").format(new Date()));
+        model.addAttribute("placeholderTextPart2", "SERVER");
+        model.addAttribute("messageTemplate", "searchbar.placeholder2");
         return "index";
     }
 }
